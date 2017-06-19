ca07367acf724e2cc530cec37c325a095132d3d0
John Van Schultz <jvansch1@binghamton.edu>
2017-06-19 19:24:00 -0400

    Initial Commit

---
+++ Bulls_and_cows/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bulls_and_cows
+//
+//  Created by john van schultz on 6/19/17.
+//  Copyright © 2017 john van schultz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
