0d2215f05968a113d65903d19bf2a15017b270c9
John Van Schultz <jvansch1@binghamton.edu>
2017-06-19 20:03:27 -0400

    Reconfigure Repo

--- Bulls_and_cows/main.cpp
+++ Bulls_and_cows/main.cpp
@@ -8,12 +8,29 @@
 
 #include <iostream>
 
+// include string
+#include <string>
+
 using namespace std;
 
 int main() {
+    // introduce the game
+    
     // Use constants for variables that do not change
+    
     constexpr int WORD_LENGTH = 9;
-    cout << "Welcome to Bulls and Cows" << endl;
-    cout << "Can you guess the " << WORD_LENGTH << " letter isogram im thinking of" << endl;
+    cout << "Welcome to Bulls and Cows\n";
+    cout << "Can you guess the " << WORD_LENGTH << " letter isogram im thinking of\n";
+    
+    // get a guess from the player
+    
+    string player_input = "";
+    cout << "Please enter your guess: ";
+    cin >> player_input;
+    
+    // repeat guess
+    
+    cout << "You guessed: " << player_input << endl;
+
     return 0;
 }
